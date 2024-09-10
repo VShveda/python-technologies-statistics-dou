1ca0ca11e0287a58bc7f02b21e19c054784c509c
Viktor Sveda <vinix.ru@gmail.com>
2024-09-10 18:56:46 +0300

    set up and uploaded files for scraper

--- work_scraper/middlewares.py
+++ work_scraper/middlewares.py
@@ -9,7 +9,7 @@ from scrapy import signals
 from itemadapter import is_item, ItemAdapter
 
 
-class WorkSkraperSpiderMiddleware:
+class WorkScraperSpiderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the spider middleware does not modify the
     # passed objects.
@@ -56,7 +56,7 @@ class WorkSkraperSpiderMiddleware:
         spider.logger.info("Spider opened: %s" % spider.name)
 
 
-class WorkSkraperDownloaderMiddleware:
+class WorkScraperDownloaderMiddleware:
     # Not all methods need to be defined. If a method is not defined,
     # scrapy acts as if the downloader middleware does not modify the
     # passed objects.
